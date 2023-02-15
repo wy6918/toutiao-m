3a6a26954467b084a64b7ef3050db769a9f8b409
wuyun <wuyun@itcast.cn>
2023-02-15 16:05:55 +0800

    引入vant和icon

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <div id="app">
     <router-view/>
     <div>黑马头条</div>
+    <div>
+      <i class="toutiao icon-fenxiang"></i>
+    </div>
+    <div>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+    </div>
   </div>
 </template>
 
